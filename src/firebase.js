02e1c642e9c1a554d1df3d33e8d1bc2946077ea9
Alloh Nkefor Rodrick <allohnkeforrodrick@gmail.com>
2024-08-16 12:02:16 +0100

    This is the first hour of work

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCQ-OUJl1p-QXBvQP685ZW3F9XjLDbYFHo",
+    authDomain: "whatsapp-clone-bb9ae.firebaseapp.com",
+    projectId: "whatsapp-clone-bb9ae",
+    storageBucket: "whatsapp-clone-bb9ae.appspot.com",
+    messagingSenderId: "567994739566",
+    appId: "1:567994739566:web:9659a14671feeb0e1def7f"
+  };
