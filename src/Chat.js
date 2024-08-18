4af2cfc3654d1ce0667c216917feed4d9bd2b9ba
Alloh Nkefor Rodrick <allohnkeforrodrick@gmail.com>
2024-08-18 20:13:38 +0100

    final touches on the frontend portion, part one

--- src/Chat.js
+++ src/Chat.js
@@ -1,10 +1,61 @@
 import React from 'react';
 import './Chat.css';
+import { Avatar, IconButton } from '@mui/material';
+import { AttachFile, InsertEmoticon, MoreVert, SearchOutlined } from '@mui/icons-material';
 
 function Chat() {
   return (
     <div className='chat'>
-      <h1>I am the chat</h1>
+      <div className='chat_header'>
+        <Avatar />
+
+        <div className='chat_headerInfo'>
+          <h3>Room name</h3>
+          <p>Last seen at...</p>
+        </div>
+
+        <div className='chat_headerRight'>
+          <IconButton>
+            <SearchOutlined />
+          </IconButton>
+          <IconButton>
+            <AttachFile />
+          </IconButton>
+          <IconButton>
+            <MoreVert />
+          </IconButton>
+        </div>
+      </div>
+
+      <div className='chat_body'>
+        <p className='chat_message'>
+          <span className='chat_name'>Sonny</span>
+          This is a message
+          <span className='chat_timestamp'>
+            {new Date().toUTCString()}
+          </span>
+        </p>
+
+        <p className='chat_receiver chat_message'>
+          <span className='chat_name'>Sonny</span>
+          This is a message
+          <span className='chat_timestamp'>
+            {new Date().toUTCString()}
+          </span>
+        </p>
+      </div>
+
+      <div className='chat_footer'>
+        <InsertEmoticon />
+        <form>
+          <input
+          type='text'
+          placeholder='Type a message'
+          />
+          <button type="submit">Send a message</button>
+        </form>
+          
+      </div>
     </div>
   )
 }
--- src/Sidebar.js
+++ src/Sidebar.js
@@ -4,6 +4,8 @@ import DonutLargeIcon from "@mui/icons-material/DonutLarge";
 import ChatIcon from "@mui/icons-material/Chat";
 import MoreVertIcon from "@mui/icons-material/MoreVert";
 import { Avatar, IconButton } from '@mui/material';
+import { SearchOutlined } from '@mui/icons-material';
+import SidebarChat from './SidebarChat';
 
 function Sidebar() {
   return (
@@ -22,8 +24,22 @@ function Sidebar() {
             </IconButton>
         </div>
       </div>
+
+      <div className='sidebar_search'>
+        <div className='sidebar_searchContainer'>
+          <SearchOutlined />
+          <input placeholder='Search or start new chat' type='text' />
+        </div>
+      </div>
+
+      <div className='sidebar_chats'>
+        <SidebarChat />
+        <SidebarChat />
+        <SidebarChat />
+      </div>
+
     </div>
-  )
+  );
 }
 
 export default Sidebar
